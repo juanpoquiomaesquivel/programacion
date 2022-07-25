acfa7a95ba0aa53e38fb3020c06ca080ed441851
jojoxjohn <juan.poquioma@unmsm.edu.pe>
2022-07-25 09:29:02 -0500

    java

---
+++ Java/discoduroderoer/ejercicio1.java
@@ -0,0 +1,19 @@
+/*public class ejercicio1 {
+
+    public static void main(String[] args) {
+        int var1 = 166;
+        int var2 = 81;
+        int suma = var1 + var2;
+        int resta = var1 - var2;
+        int multiplicacion = var1 * var2;
+        float division = (float) var1 / var2;
+        int modulo = var1 % var2;
+
+        System.out.println("a = " + var1 + " y b = " + var2);
+        System.out.println("suma = " + suma);
+        System.out.println("resta = " + resta);
+        System.out.println("multiplicacion = " + multiplicacion);
+        System.out.println("division = " + division);
+        System.out.println("modulo = " + modulo);
+    }
+}*/
---
+++ Java/discoduroderoer/ejercicio2.java
@@ -0,0 +1,15 @@
+/*public class ejercicio2 {
+
+    public static void main(String[] args) {
+        int n1 = 84;
+        int n2 = 12;
+
+        if (n1 > n2) {
+            System.out.println("El numero " + n1 + " es mayor que " + n2 + ".");
+        } else if (n1 < n2) {
+            System.out.println("El numero " + n2 + " es mayor que " + n1 + ".");
+        } else {
+            System.out.println("Ambos numeros son iguales.");
+        }
+    }
+}*/
