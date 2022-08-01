104b777697a1a8823fad476db18a23d2a37078a7
jojoxjohn <juan.poquioma@unmsm.edu.pe>
2022-08-01 03:20:25 -0500

    +ejercicios hoy

---
+++ C/Fernando Berzal/ejercicio4a-f.c
@@ -0,0 +1,19 @@
+/*#include <stdio.h>
+
+int main()
+{
+    int x = 1;
+    printf("a =>: %d\n", (x > 1) & (x++ < 10));
+    x = 1;
+    printf("b =>: %d\n", (1 > x) && (1 > x++));
+    x = 1;
+    printf("c =>: %d\n", (1 == x) | (10 > x++));
+    x = 1;
+    printf("d =>: %d\n", (1 == x) || (10 > x++));
+    x = 1;
+    printf("e =>: %d\n", (++x) + x);
+    x = 1;
+    printf("f =>: %d\n", x + (++x));
+
+    return 0;
+}*/
---
+++ Java/discoduroderoer/ejercicio5.java
@@ -0,0 +1,15 @@
+/*import java.util.Scanner;
+
+public class ejercicio5 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double R, area;
+
+        System.out.print("Ingrese el valor de 'R': ");
+        R = entrada.nextDouble();
+
+        area = Math.PI * Math.pow(R, 2);
+        System.out.println("El area del circulo es: " + area);
+    }
+}*/
