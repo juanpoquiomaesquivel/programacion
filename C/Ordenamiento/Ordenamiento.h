f0944471be002ca7fc37fcfd61e946aa6904a272
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2022-09-13 23:17:04 -0500

    Solo le cambié el nombre a la carpeta

---
+++ C/Ordenamiento/Ordenamiento.h
@@ -0,0 +1,19 @@
+#ifndef _ORDENAMIENTO_H_
+#define _ORDENAMIENTO_H_
+
+#include "Libreria.h"
+
+int *burbuja(int *vector, int n);
+int *insercionDirecta(int *vector, int n);
+int *seleccionDirecta(int *vector, int n);
+int *shell(int *vector, int n);
+void reduceQuickSort(int *subVector, int inicio, int fin);
+int *quickSort(int *vector, int n);
+void swap(int *a, int *b);
+void heapify(int *subVector, int n, int i);
+int *heapSort(int *vector, int n);
+void merge(int *subVector, int l, int m, int r);
+void mergeSorting(int *subVector, int l, int r);
+int *mergeSort(int *vector, int n);
+
+#endif
